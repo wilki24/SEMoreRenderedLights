b19b535d02e8e111e41ed395f6e00047ff75f188
wilki24 <8873894+wilki24@users.noreply.github.com>
2022-05-26 14:43:26 -0700

    Basic Framework also updated harmony version

---
+++ ClientPlugin/Patches/MaxSpotlightsPatch.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPlugin.Patches
+{
+    [HarmonyPatch]
+    class MaxSpotlightsPatch
+    {
+        [HarmonyPrefix]
+        private static void Prefix()
+        {
+            // This method returns immediately if the number of spotlights is 32 or less. So, we'll
+            // just always return to allow for infinite spotlights. TODO: Make this adjustable and 
+            // use a transpiler
+            return;
+        }
+
+        [HarmonyTargetMethod]
+        public static MethodBase Target()
+        {
+            return AccessTools.Method("VRage.Render11.LightingStage.MyLightsRendering:CullSpotLights");
+        }
+    }
+
+    [HarmonyPatch]
+    class RenderSpotlightsPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                yield return instruction;
+            }
+        }
+
+        [HarmonyTargetMethod]
+        public static MethodBase Target()
+        {
+            return AccessTools.Method("VRage.Render11.LightingStage.MyLightsRendering:RenderSpotlights");
+        }
+
+    }
+}
